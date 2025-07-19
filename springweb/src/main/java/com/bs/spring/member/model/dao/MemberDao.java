ff00ecf2c0c0036a4c4773c46fa204a9b841c6f6
parksorry96 <154280243+parksorry96@users.noreply.github.com>
2025-07-19 17:58:30 +0900

    Delete springweb directory

--- springweb/src/main/java/com/bs/spring/member/model/dao/MemberDao.java
+++
@@ -1,9 +0,0 @@
-package com.bs.spring.member.model.dao;
-
-import com.bs.spring.member.model.dto.Member;
-import org.apache.ibatis.session.SqlSession;
-
-public interface MemberDao {
-    Member searchById(SqlSession session, String userId);
-    int enrollMember(SqlSession session, Member member);
-}
